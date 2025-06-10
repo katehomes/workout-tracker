b1963166bceca833c13c38b18877799a308bb16a
katehomes <76965529+katehomes@users.noreply.github.com>
2025-06-10 14:01:41 -0400

    remove unused import

--- client/src/components/exercise/ExerciseList.tsx
+++ client/src/components/exercise/ExerciseList.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { getAllExercises, type Exercise } from '../../api/exerciseApi';
-import { Link } from 'react-router-dom';
 import { useExerciseFilter } from '../../contexts/ExerciseFilterContext';
 import { getDifficultyClass } from './ExerciseFilter';
 
